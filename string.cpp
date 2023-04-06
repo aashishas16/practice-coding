c356f5c48307bccf236294262de20f0aba673e17
unknown <aashishnimcet2022@gmail.com>
2023-04-06 12:50:55 +0530

    aas

---
+++ string.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+char str[20];
+cin>>str;
+gets(str);
+fgets(str,20,stdin);
+cout<<"name is "<<str;
+   
+
+
+
+
+
+//     char str[10]={"indore"};
+//   int i;
+
+//   for(i=0;str[i];i++);
+//   cout<<i;
+  
+ 
+ //for(i=0;i<=9;i++).....
+ //for(i=0;str[i];i++).....
+
+    return 0;
+}
